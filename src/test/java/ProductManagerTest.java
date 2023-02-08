a61ea214a101cf3307c85af9cae3ead1eeded147
Viktor <115978271+ViktorDee@users.noreply.github.com>
2023-02-08 17:14:18 +0700

    Updated ProductManagerTest with new tests

--- src/test/java/ProductManagerTest.java
+++ src/test/java/ProductManagerTest.java
@@ -12,6 +12,14 @@ public class ProductManagerTest {
     Product product3 = new Smartphone(45, "iphone 12", 34800, "china");
     Product product4 = new Product(41, "футболка", 450);
     Product product5 = new Product(56, "торт", 299);
+    Product product6 = new Product(31, "футболка красная", 4990);
+    Product product7 = new Product(21, "футболка с длинным рукавом", 440);
+    Product product8 = new Product(11, "футболка с принтом", 350);
+    Product product9 = new Smartphone(455, "iphone 12", 31500, "china");
+    Product product10 = new Smartphone(123, "iphone 12", 14900, "china");
+    Product product11 = new Book(222, "весна", 210, "ницше");
+    Product product12 = new Book(111, "осень", 320, "есенин");
+    Product product13 = new Smartphone(13, "nokia", 14100, "japan");
 
     @BeforeEach
     public void setup() {
@@ -20,12 +28,18 @@ public class ProductManagerTest {
         manager.add(product3);
         manager.add(product4);
         manager.add(product5);
+        manager.add(product6);
+        manager.add(product7);
+        manager.add(product8);
+        manager.add(product9);
+        manager.add(product10);
+        manager.add(product11);
+        manager.add(product12);
+        manager.add(product13);
     }
 
     @Test
     public void shouldFindProduct() {
-
-        manager.matches(product5, "торт");
 
         Product[] expected = {product5};
         Product[] actual = manager.searchBy("торт");
@@ -35,9 +49,28 @@ public class ProductManagerTest {
     }
 
     @Test
+    public void ShouldFindMultipleProducts() {
+
+
+        Product[] expected = {product3, product9, product10};
+        Product[] actual = manager.searchBy("iphone 12");
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotFindProducts() {
+
+        Product[] expected = {};
+        Product[] actual = manager.searchBy("");
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
     public void shouldFindBook() {
 
-        manager.matches(product2, "рассказы");
 
         Product[] expected = {product2};
         Product[] actual = manager.searchBy("рассказы");
@@ -49,10 +82,8 @@ public class ProductManagerTest {
     @Test
     public void shouldFindBookByAuthor() {
 
-        manager.matches(product2, "пушкин");
-
         Product[] expected = {product2};
-        Product[] actual = manager.searchBy("рассказы");
+        Product[] actual = manager.searchBy("пушкин");
 
         Assertions.assertArrayEquals(expected, actual);
 
@@ -61,10 +92,8 @@ public class ProductManagerTest {
     @Test
     public void shouldFindSmartphone() {
 
-        manager.matches(product3, "iphone12");
-
-        Product[] expected = {product3};
-        Product[] actual = manager.searchBy("iphone 12");
+        Product[] expected = {product13};
+        Product[] actual = manager.searchBy("nokia");
 
         Assertions.assertArrayEquals(expected, actual);
 
@@ -73,10 +102,8 @@ public class ProductManagerTest {
     @Test
     public void shouldFindSmartphoneByManufacturer() {
 
-        manager.matches(product3, "china");
-
-        Product[] expected = {product3};
-        Product[] actual = manager.searchBy("iphone 12");
+        Product[] expected = {product13};
+        Product[] actual = manager.searchBy("japan");
 
         Assertions.assertArrayEquals(expected, actual);
 
@@ -85,12 +112,11 @@ public class ProductManagerTest {
     @Test
     public void shouldFindProductsWithTheSameName() {
 
-        manager.matches(product1, "футболка");
-
         Product[] expected = {product1, product4};
         Product[] actual = manager.searchBy("футболка");
 
         Assertions.assertArrayEquals(expected, actual);
 
     }
+
 }
